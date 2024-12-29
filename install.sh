ff1947f77be6e8dbd417b989cfd261ba8ea726dd
AleRosmo <onair95@gmail.com>
2024-12-29 09:01:44 +0100

    remove unused ini files

--- install.sh
+++ install.sh
@@ -22,7 +22,6 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 
 # TODO: Do we really need to add Docker's repository? Can't we just install from Debian repo?
 # Add Docker repository
-
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
