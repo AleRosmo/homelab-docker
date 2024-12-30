8f24c305b876ef651110a1c5476ec0cbe9f57bc6
AleRosmo <onair95@gmail.com>
2024-12-30 02:45:58 +0100

    add reminder

--- influxdb/init-script.sh
+++ influxdb/init-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+#TODO: Add your secure token, because there is no automated way yet to do it
 influx bucket create --name water_heating_data --retention 30d
 influx auth create --write-bucket water_heating_data --read-bucket water_heating_data --token YOUR_SECURE_TOKEN
